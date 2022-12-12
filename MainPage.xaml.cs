bca7a710517dee03f6edb64d7ff4c187b90a187d
saipotla123 <114969205+saipotla123@users.noreply.github.com>
2022-12-11 20:18:14 -0600

    Add files via upload

---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using TaskManager.ViewModel;
+
+namespace TaskManager;
+
+public partial class MainPage : ContentPage
+{
+	
+
+	public MainPage(MainViewModel vm)
+	{
+		InitializeComponent();
+		BindingContext = vm;
+	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+}
+
